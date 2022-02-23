7d3b2ebd5df02b38614d228a9df7b5a85ea82736
The Crypto Kraken <lokesh@gfuturetech.com>
2022-02-23 15:18:46 +0530

    added erc20 contract and hardhat setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,33 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
-};
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+    }
+  },
+  solidity: {
+    version: "0.7.5",
+    settings: {
+      metadata: {
+        bytecodeHash: "none",
+      },
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
